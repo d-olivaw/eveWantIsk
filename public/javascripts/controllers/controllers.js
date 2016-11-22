f5cfd76d0fe81d87502152e84184db594b7e8e8b
Jeff <medders.jeffrey@gmail.com>
2016-11-22 13:16:03 -0700

    finding jita buy logic

--- public/javascripts/controllers/controllers.js
+++ public/javascripts/controllers/controllers.js
@@ -2,16 +2,33 @@ app.controller("eveController", ["$scope", "eveService", function($scope, eveSer
   $scope.view = {};
   $scope.view.hello = "hello";
   $scope.view.hide = false;
-  $scope.view.showMe = function(){
-    eveService.grabItems().then(function(data){
-      var itemInfo = data['data']['items'];
-      for (var i = 0; i < itemInfo.length; i++) {
-        if(itemInfo[i]['type']['id'] == 5439){
-          console.log(itemInfo[i]);
-          $scope.view.hide = true;
-          $scope.view.info = itemInfo[i];
-        }
+  $scope.view.regionId = 10000002;
+  $scope.view.itemId = 5439;
+  $scope.view.stationId = 60003760;
+  eveService.grabSell($scope.view.regionId, $scope.view.itemId).then(function(data){
+    // console.log(data['data']['items'][0]);
+  });
+  eveService.grabBuy($scope.view.regionId, $scope.view.itemId).then(function(data){
+    var getinnest = data['data']['items']
+    var newArr = [];
+    for (var i = 0; i < getinnest.length; i++) {
+      if(getinnest[i]['location']['id'] == $scope.view.stationId){
+        newArr.push(getinnest[i]['price'])
       }
-    })
-  }
+    }
+    var maxBuy = Math.max(...newArr);
+    console.log('jita-buy', maxBuy);
+  });
+  // $scope.view.showMe = function(){
+  //   eveService.grabItemsById().then(function(data){
+  //     var itemInfo = data['data']['items'];
+  //     for (var i = 0; i < itemInfo.length; i++) {
+  //       if(itemInfo[i]['type']['id'] == 5439){
+  //         console.log(itemInfo[i]);
+  //         $scope.view.hide = true;
+  //         $scope.view.info = itemInfo[i];
+  //       }
+  //     }
+  //   })
+  // }
 }])
--- public/javascripts/services/services.js
+++ public/javascripts/services/services.js
@@ -1,7 +1,13 @@
 app.factory('eveService', function($http) {
   return {
-    grabItems: function(){
+    grabItemsById: function(){
       return $http.get('https://crest-tq.eveonline.com/market/prices/')
-    }
+    },
+    grabSell: function(regionId, itemId){
+      return $http.get('https://crest-tq.eveonline.com/market/'+ regionId + '/orders/sell/?type=https://crest-tq.eveonline.com/inventory/types/' + itemId + '/')
+    },
+    grabBuy: function(regionId, itemId){
+      return $http.get('https://crest-tq.eveonline.com/market/'+ regionId + '/orders/buy/?type=https://crest-tq.eveonline.com/inventory/types/' + itemId + '/')
+    },
   }
 });
