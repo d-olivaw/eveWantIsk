dfcd7e05b21bbfc5537df7174d37d5c3538e15f0
Jeff <medders.jeffrey@gmail.com>
2016-11-23 16:32:55 -0700

    build objects for items

--- public/javascripts/services/services.js
+++ public/javascripts/services/services.js
@@ -1,12 +1,15 @@
 app.factory('eveService', function($http) {
   return {
     grabItemsById: function(){
+      //we're not using this for now. could get average value
       return $http.get('https://crest-tq.eveonline.com/market/prices/')
     },
     grabSell: function(regionId, itemId){
+      //grab sell prices for item at correct regionId
       return $http.get('https://crest-tq.eveonline.com/market/'+ regionId + '/orders/sell/?type=https://crest-tq.eveonline.com/inventory/types/' + itemId + '/')
     },
     grabBuy: function(regionId, itemId){
+      //grab buy prices for item at correct regionid
       return $http.get('https://crest-tq.eveonline.com/market/'+ regionId + '/orders/buy/?type=https://crest-tq.eveonline.com/inventory/types/' + itemId + '/')
     },
   }
