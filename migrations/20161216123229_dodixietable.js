45bb3ea3d218c6f47014a3b44e3c15078b73c642
Jeff <medders.jeffrey@gmail.com>
2016-12-16 12:38:17 -0700

    created db tables for regions

---
+++ migrations/20161216123229_dodixietable.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('dodixie', function(table){
+    table.increments('id').primary();
+    table.string('regionId').notNullable();
+    table.string('stationId').notNullable();
+    table.integer('itemId').notNullable().unique();
+    table.bigInteger('volumeSell').notNullable();
+    table.bigInteger('buy').notNullable();
+    table.bigInteger('sell').notNullable();
+    table.integer('markup').notNullable();
+    table.bigInteger('histAvg').notNullable();
+    table.bigInteger('profitPotential').notNullable();
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('dodixie');
+};
